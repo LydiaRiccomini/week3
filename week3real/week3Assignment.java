16e97bd3882f688d04dde98d829205fd53bdf880
Lydia Riccomini <104948030+LydiaRiccomini@users.noreply.github.com>
2022-05-22 01:23:27 -0500

    finish step 1

--- week3real/week3Assignment.java
+++ week3real/week3Assignment.java
@@ -2,4 +2,41 @@ package week3real;
 
 public class week3Assignment {
 
+	static void step1(){
+		int[] ages = {
+				3,9,23,64,2,8,28,93
+		};
+		
+		System.out.println(ages[ages.length -1] - ages[0]);
+		
+		int[] newAges = {
+				3,9,23,64,2,8,28,93,42
+		};
+		
+		
+		System.out.println(newAges[newAges.length - 1] - newAges[0]);
+		
+		int i;
+		int totalAge = 0;
+		
+		for(i = 0; i < ages.length; i++) {
+			totalAge += ages[i];
+		}
+		
+		System.out.println(totalAge / ages.length);
+		
+	}
+	
+	static void step2() {
+		
+	}
+	
+	
+	
+	public static void main(String[] aarg) {
+		step1();  
+		
+	}
+	
+	
 }
